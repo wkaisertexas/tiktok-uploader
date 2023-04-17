e4a78444ec3f8bf0296b2f76d9b124278079abc2
William Kaiser <wkaisertexas@gmail.com>
2023-04-16 23:57:50 -0400

    added a basic examples upload

---
+++ examples/basic_upload.py
@@ -0,0 +1,17 @@
+"""Gets a video from the internet and uplaods it"""
+
+import urllib.request
+
+from tiktok_uploader.upload import upload_video
+
+URL = "https://raw.githubusercontent.com/wkaisertexas/wkaisertexas.github.io/main/upload.mp4"
+FILENAME = "upload.mp4"
+
+if __name__ == "__main__":
+    # download random video
+    urllib.request.urlretrieve(URL, FILENAME)
+    
+    # upload video to TikTok
+    upload_video(FILENAME,
+                 description="This is a video I just downloaded",
+                 cookies="cookies.txt")
